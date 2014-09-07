cb0246107b421ec5b8bba1e0efbf8c2ad808fd88
Stas Ukolov <ukoloff@gmail.com>
2014-09-07 13:16:20 +0600

    New gem

---
+++ lib/rufus/lua/moon.rb
@@ -0,0 +1,9 @@
+require "rufus/lua/moon/version"
+
+module Rufus
+  module Lua
+    module Moon
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rufus/lua/moon/version.rb
@@ -0,0 +1,7 @@
+module Rufus
+  module Lua
+    module Moon
+      VERSION = "0.0.1"
+    end
+  end
+end
