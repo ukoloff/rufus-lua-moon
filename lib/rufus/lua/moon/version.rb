e8fe535fd1b49555b23a580078c32f404b0a154b
Stas Ukolov <ukoloff@gmail.com>
2014-09-15 12:42:26 +0600

    Handle (pre)release in version

--- lib/rufus/lua/moon/version.rb
+++ lib/rufus/lua/moon/version.rb
@@ -2,8 +2,8 @@ module Rufus
   module Lua
     module Moon
       Path=File.expand_path '../../../../../vendor/lua', __FILE__
-      m=/(["'])(\d+(\.\d+){1,3})\1/.match File.read File.expand_path 'moonscript/version.lua', Path
-      VERSION = m ? m[2] : '0.0.?'
+      m=/(["'])(\d+(\.\d+){1,3}(-(?!0)[\da-z]))\1/i.match File.read File.expand_path 'moonscript/version.lua', Path
+      VERSION = m ? m[2].gsub('-', '.') : '0.0.?'
     end
   end
 end
