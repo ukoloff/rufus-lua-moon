0f0e7877974382c099a76acbafb67d728d97a0b0
Stas Ukolov <ukoloff@gmail.com>
2014-09-15 14:43:40 +0600

    Fix for missing preprelease

--- lib/rufus/lua/moon/version.rb
+++ lib/rufus/lua/moon/version.rb
@@ -5,7 +5,7 @@ module Rufus
       subV=File.read(File.expand_path '.subversion', root).strip rescue ''
       subV="."+subV.gsub(/\W+/, '.') if subV.length>0
       Path=File.expand_path 'vendor/lua', root
-      m=/(["'])(\d+(\.\d+){1,3}(-(?!0)[\da-z]))\1/i.match File.read File.expand_path 'moonscript/version.lua', Path
+      m=/(["'])(\d+(\.\d+){1,3}(-(?!0)[\da-z])*)\1/i.match File.read File.expand_path 'moonscript/version.lua', Path
       VERSION = m ? m[2].gsub('-', '.')+subV : '0.0.?'
     end
   end
