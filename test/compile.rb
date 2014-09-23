90f1bbcb84232195581988862056656a4549d1d0
Stas Ukolov <ukoloff@gmail.com>
2014-09-23 12:24:54 +0600

    MoonScript before v0.2.4 raises error

--- test/compile.rb
+++ test/compile.rb
@@ -1,13 +1,12 @@
 class TestC < Minitest::Test
   def test_compile
     s=Rufus::Lua::State.new.moon!
-    assert s.eval("return require('moonscript').to_lua('a b c')")[0]
-      .gsub(/\s+/, '')['a(b(c))']
-
+    assert s.eval("return require('moonscript').to_lua('x = a b c')")[0] 
+      .gsub(/\s+/, '')['=a(b(c))']
   end
 
   def test_fail
     s=Rufus::Lua::State.new.moon!
-    refute s.eval("return require('moonscript').to_lua 'a=b:'")[0]
+    refute s.eval("return require('moonscript').to_lua 'a=b:'")[0] rescue nil
   end
 end
