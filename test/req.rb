79a23ba50eead78f14ce3a261111a8c72bec86a9
Stas Ukolov <ukoloff@gmail.com>
2014-09-23 10:37:06 +0600

    Fail on bad component

--- test/req.rb
+++ test/req.rb
@@ -7,4 +7,11 @@ class TestV < Minitest::Test
     }.map{|k, v|([nil]+v).map{|i| [k, i].compact*'.'}}
     .flatten.each {|f| s.eval "require '#{f}'"}
   end
+
+  def test_missing
+    assert_raises(Rufus::Lua::LuaError) do
+      s=Rufus::Lua::State.new.moon!
+      s.eval "require 'moonscript.oops'"
+    end
+  end
 end
