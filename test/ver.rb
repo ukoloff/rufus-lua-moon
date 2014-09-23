7f41fac163d22d89234de87d0382c76229afcc1b
Stas Ukolov <ukoloff@gmail.com>
2014-09-23 10:15:26 +0600

    Check version string

--- test/ver.rb
+++ test/ver.rb
@@ -1,11 +1,19 @@
 class TestV < Minitest::Test
   def test_ver
+    s=Rufus::Lua::State.new.moon!
+    luav=s.eval 'return _VERSION'
+    rufv=Rufus::Lua::VERSION
+    moonv=s.eval 'return require("moonscript.version").version'
+    gemv=Rufus::Lua::Moon::VERSION
+
+    len=moonv.length
+
+    assert_equal gemv[0...len], moonv.gsub('-', '.')
+    c=gemv[len]
+    assert(c.nil? || '.'==c)
+
     at_exit do
-      s=Rufus::Lua::State.new.moon!
-      luav=s.eval 'return _VERSION'
-      rufv=Rufus::Lua::VERSION
-      moonv=s.eval 'return require("moonscript.version").version'
-      gemv=Rufus::Lua::Moon::VERSION
+      puts
       puts "Lua version #{luav}"
       puts "Rufus::Lua version #{rufv}"
       puts "MoonScript version #{moonv}"
