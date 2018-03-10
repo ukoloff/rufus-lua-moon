0a414d7b4baddf956200b36a3c0019aff706f8c1
Stas Ukolov <ukoloff@gmail.com>
2018-03-10 09:00:07 +0500

    Use threads

--- test/appveyor.rb
+++ test/appveyor.rb
@@ -2,11 +2,14 @@ class TestAV < Minitest::Test
   def test_av
     # return unless defined? AppVeyor::Worker
     1000.times do |i|
+      sleep 0.01
+      Thread.new do
 	::AppVeyor::Worker.test testFramework: 'None',
             testName: "Test #{i}",
             fileName: "none.rb:#{i+1}",
             outcome: 'Passed', 
             durationMilliseconds: 108
+      end
     end
   end
 end
