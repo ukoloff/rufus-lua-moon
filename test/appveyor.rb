0df0984bdea1799d6e402e9cc03efdc8c61aa25c
Stas Ukolov <ukoloff@gmail.com>
2018-03-10 09:37:44 +0500

    Batch report

--- test/appveyor.rb
+++ test/appveyor.rb
@@ -1,15 +1,24 @@
 class TestAV < Minitest::Test
   def test_av
     # return unless defined? AppVeyor::Worker
-    1000.times do |i|
-      sleep 0.01
-      #Thread.new do
+    ::AppVeyor::Worker.tests (1..1000).map do |i|
 	::AppVeyor::Worker.test testFramework: 'None',
             testName: "Test #{i}",
             fileName: "none.rb:#{i+1}",
             outcome: 'Passed', 
             durationMilliseconds: 108
-      #end
     end
+  end
+end
+
+
+module AppVeyor::Worker
+  def self.tests info
+    x = api or return
+    body = JSON.generate info
+    x.post '/api/tests/batch',
+      body,
+      'Content-Length'=>body.length.to_s,
+      'Content-Type'=>'application/json'
   end
 end
