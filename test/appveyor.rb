92f2d7129b2e79b4ed17996c3dc6d3848388c338
Stas Ukolov <ukoloff@gmail.com>
2018-03-10 09:55:09 +0500

    Print body

--- test/appveyor.rb
+++ test/appveyor.rb
@@ -21,7 +21,7 @@ module AppVeyor::Worker
   def self.tests info
     x = api or return
     body = JSON.generate info
-    message body
+    puts body
     x.put '/api/tests/batch',
       body,
       'Content-Length'=>body.length.to_s,
